0f5ce3fb1e70ad5ca9a30a3ffeac7ca1f5a7aba5
Gilberto López <gilisaac@ciencias.unam.mx>
2018-06-07 18:01:05 -0500

    Correccion: se escoge el subconjunto de entrenamiento de manera aleatoria del dataset original

--- models.py
+++ models.py
@@ -21,18 +21,25 @@ from keras.regularizers import l2
 # Cargar datos
 # =============================================================================
 
+# Dataset completo
 X,y = load_dataset()
 
-train_size = int(np.floor(X.shape[0]*.7))
-test_size = X.shape[0] - train_size
+total_size = X.shape[0]
+train_size = int(np.floor(total_size*.8))
+test_size = total_size - train_size
 
-print('- Dataset: %d samples' % X.shape[0])
+print('- Dataset: %d samples' % total_size)
 print('- Training set: %d samples' % train_size)
 print('- Test set: %d samples' % test_size)
 
-# Datos a usar
-X_train,y_train = X[:train_size],y[:train_size]
-X_test,y_test = X[train_size:],y[train_size:]
+np.random.seed(54)
+rp = np.random.permutation(total_size)
+idx_train = rp[:train_size]
+idx_test = rp[train_size:]
+
+# Datos a usar para entrenamiento y verificacion
+X_train,y_train = X[idx_train],y[idx_train]
+X_test,y_test = X[idx_test],y[idx_test]
 
 # =============================================================================
 # Clasificador 1
@@ -41,40 +48,48 @@ X_test,y_test = X[train_size:],y[train_size:]
 classifier_1 = Sequential()
 
 classifier_1.add(Conv2D(filters = 100,
-                        kernel_size = (127,10),
-                        strides = (1,3),
+                        kernel_size = (127,15),
+                        strides = (1,5),
                         input_shape = (127, 2900, 1),
-                        activation = 'relu'))
-classifier_1.add(MaxPooling2D(pool_size = (1,3)))
-
-classifier_1.add(Conv2D(filters = 30,
-                        kernel_size = (1,4),
-                        strides = (1,2),
                         activation = 'relu'))
 classifier_1.add(MaxPooling2D(pool_size = (1,2)))
 
 classifier_1.add(Dropout(.5))
 
+#classifier_1.add(Conv2D(filters = 64,
+#                        kernel_size = (1,15),
+#                        strides = (1,5),
+#                        activation = 'relu'))
+#classifier_1.add(MaxPooling2D(pool_size = (1,5)))
+#
+#classifier_1.add(Dropout(.5))
+
 classifier_1.add(Flatten())
 
-classifier_1.add(Dense(64, activation = 'relu'))
+classifier_1.add(Dense(128, activation = 'relu'))
 
-classifier_1.add(Dense(64, activation = 'relu'))
+classifier_1.add(Dropout(.2))
 
 classifier_1.add(Dense(10, activation = 'softmax',
                        kernel_regularizer = l2(0.0001)))
 
 classifier_1.compile(optimizer = Adam(0.0001),
-                     loss = 'categorical_crossentropy', metrics = ['accuracy'])
+                     loss = 'categorical_crossentropy',
+                     metrics = ['accuracy'])
 
 # =============================================================================
 # Entrenamiento
 # =============================================================================
 
+# Bagging, cada modelo se entrena en una muestra con reemplazo del dataset de
+# entrenamiento, se evalua con el conjunto de verificacion
 Xb,yb = bootstrap_set(X_train,y_train,
-                      int(np.floor(train_size*.8)))
+                      int(np.floor(train_size*.9)))
 
-classifier_1.fit(Xb,yb,batch_size = 64,epochs = 15,
+classifier_1.fit(Xb,yb,batch_size = 32,epochs = 7,
                  validation_data = (X_test,y_test))
 
-classifier_1.save('classifier_1')
+L = classifier_1.predict(X_test)
+cls = np.argmax(L,axis = 1)
+
+classifier_1.save('classifier_1.h5')
