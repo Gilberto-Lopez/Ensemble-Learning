b0e8addc3f6fb8cc7ee259fe28c892937760a600
Gilberto López <gilisaac@ciencias.unam.mx>
2018-06-07 20:17:13 -0500

    load_dataset es una funcion fuera de Ensemble.

--- Ensemble.py
+++ Ensemble.py
@@ -181,40 +181,6 @@ class Ensemble (object):
     return ensemble
 
   @staticmethod
-  def load_dataset ():
-    """Carga el dataset de imagenes de espectrogramas que estan en el directorio
-    "genres/".
-
-    Regresa:
-    --------
-    X : np.array
-      Las imagenes de tamaño (127*2900).
-    y : np.array
-      Las etiquetas de las imagenes.
-    """
-    y = []
-    X = []
-    i = 0
-
-    for root,_,files in os.walk('genres/'):
-      if len(files) != 0:
-        l = [0]*10
-        l[i] = 1
-        y = y + [l]*len(files)
-        i += 1
-      for file in files:
-        path = os.path.join(root,file)
-        img = plt.imread(path)
-        if img.shape != (127,2900):
-          img = misc.imresize(img,(127,2900))
-        X.append(np.expand_dims(img,2))
-
-    X = np.array(X)
-    y = np.array(y)
-
-    return X,y
-
-  @staticmethod
   def bootstrap_set (S, labels, size, seed = None):
     """Genera una muestra aleatoria con remplazo de S del tamaño indicado.
 
